b9528e14519073f9f5743e01a02fb24b557cd09f
ctsictai <ctsictai@gmail.com>
2019-12-13 00:07:51 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,17 +36,17 @@ self.__precacheManifest = [
     "url": "styles-24563c4fb4475f7f0cef.js"
   },
   {
-    "url": "commons-6d28e93ba4012101c5da.js"
+    "url": "commons-b9f320896543e9578b8e.js"
   },
   {
-    "url": "app-731bbdc732d7e1133d31.js"
+    "url": "app-db6ea8f1206e7ed251ca.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-568c113b24a9e8ed8db8.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1c4d8227a199bd490fbe5ca429d910ea"
+    "revision": "30e116ddf8779018b60ed862cf34a985"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-731bbdc732d7e1133d31.js`))) {
+  if (!resources || !(await caches.match(`/app-db6ea8f1206e7ed251ca.js`))) {
     return await fetch(event.request)
   }
 
